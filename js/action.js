1def7d7596baaa75529b75319ad485a2011f2557
ethantai <ethantai@umich.edu>
2023-12-04 00:55:49 -0500

    Initial commit

---
+++ js/action.js
@@ -0,0 +1,38 @@
+
+window.addEventListener("load", function() {
+	
+    console.log("Good job opening the window")
+
+    main = document.getElementById("main");
+
+    
+
+
+	document.querySelector("#i").addEventListener("click", () => {
+		main.style.backgroundColor = "rgb(255, 0, 0)";
+		console.log("Sfdsfsd");
+    });
+
+
+	icon.addEventListener("mouseover", () => {
+        icon.style.transform = "scale(1.2)";
+    });
+
+    icon.addEventListener("mouseout", () => {
+        icon.style.transform = "scale(1)";
+    });
+	petsTitle = document.getElementById("petsTitle");
+	let isFontColorRed = false;
+    icon.addEventListener("dblclick", () => {
+        isFontColorRed = !isFontColorRed;
+        petsTitle.style.color = isFontColorRed ? "purple" : "black";
+        console.log("Font color toggled!");
+    });
+
+    
+
+
+
+
+});
+
